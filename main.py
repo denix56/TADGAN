3605d17518ec1c08175f6318d3e0e189b9fc2cd6
dsenkin <denisx9.0c@gmail.com>
2021-04-28 12:12:33 +0200

    First try trainer setup

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import torch
+import pytorch_lightning as pl
+from pytorch_lightning.plugins import DDPPlugin
 from NABTraf import NABTraf
+from tadgan import TadGAN
 
 if __name__ == '__main__':
-    pl_data = NABTraf(batch_size=1)
-    pl_data.prepare_data()
-    pl_data.setup()
-    dl = pl_data.train_dataloader()
-    print(next(iter(dl)))
+    data = NABTraf()
+    net = TadGAN(in_size=1)
+
+    trainer = pl.Trainer(plugins=[DDPPlugin(find_unused_parameters=False)], fast_dev_run=True, weights_summary='full',
+                         log_gpu_memory=True, gpus=[6, 7])
+    trainer.fit(net, datamodule=data)
